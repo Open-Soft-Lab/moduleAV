706be38d206f19290dcb6218520c41ca14cffec0
Open-Soft-Lab <clovermail120@gmail.com>
2025-02-11 19:23:39 +0300

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,12 +1,13 @@
+import os
 import disk_scanner
 import link_scanner
 
 def main_menu():
+    """Отображает главное меню программы."""
     print("Выберите режим работы:")
     print("1. Сканирование дисков")
     print("2. Сканирование ссылок")
     choice = input("Введите номер режима: ")
-
     if choice == "1":
         # Запуск сканирования дисков
         disk_scanner.main()
@@ -17,4 +18,16 @@ def main_menu():
         print("Неверный выбор. Пожалуйста, выберите 1 или 2.")
 
 if __name__ == "__main__":
-    main_menu()
+    # Константы
+    extract_to = os.path.join(os.getcwd(), "bases")  # Целевая папка
+    
+    while True:
+        # Проверяем, существует ли директория bases
+        if os.path.exists(extract_to):
+            print(f"Директория {extract_to} найдена. Переходим к главному меню.")
+            main_menu()
+            break
+        else:
+            print(f"Директория {extract_to} не найдена.")
+            input("Нажмите Enter, чтобы проверить наличие директории снова...")
+            # После нажатия Enter проверяем заново
